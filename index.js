146458fc28bc350640b24be6dcfbab375433c301
juan <luisls2129@gmail.com>
2020-11-20 11:50:48 +0100

    casi terminado

---
+++ index.js
@@ -0,0 +1,7 @@
+function ventanaModal(){
+    let elemento = document.getElementsByClassName("c-contenedor-modal")[0].style.display = "block";
+}
+
+function cerrarModal(){
+    let elemento = document.getElementsByClassName("c-contenedor-modal")[0].style.display = "none";
+}
